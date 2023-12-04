f7e002688c069bc80aa69aff7bc0755202d26194
Ahmed Frikha <ahmed.frikha@carnet.tn>
2023-12-04 16:37:10 +0100

    interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {CvComponent} from "./cv/cv/cv.component";
-import {ListComponent} from "./cv/list/list.component";
-import {ItemComponent} from "./cv/item/item.component";
-import {CardComponent} from "./cv/card/card.component";
-import {DefaultImagePipe} from "./cv/pipes/default-image.pipe";
-import {NavbarComponent} from "./components/navbar/navbar.component";
-import {HomeComponent} from "./components/home/home.component";
-import {FrontComponent} from "./components/front/front.component";
-import {AdminComponent} from "./components/admin/admin.component";
-import {MasterDetailsComponent} from "./cv/master-details/master-details.component";
-import {DetailCvComponent} from "./cv/detail-cv/detail-cv.component";
-import {NF404Component} from "./components/nf404/nf404.component";
-import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HttpClientModule} from "@angular/common/http";
-import {ToastrModule} from "ngx-toastr";
+import { CvComponent } from './cv/cv/cv.component';
+import { ListComponent } from './cv/list/list.component';
+import { ItemComponent } from './cv/item/item.component';
+import { CardComponent } from './cv/card/card.component';
+import { DefaultImagePipe } from './cv/pipes/default-image.pipe';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FrontComponent } from './components/front/front.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { MasterDetailsComponent } from './cv/master-details/master-details.component';
+import { DetailCvComponent } from './cv/detail-cv/detail-cv.component';
+import { NF404Component } from './components/nf404/nf404.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 import { ListCvEmbaucheComponent } from './cv/list-cv-embauche/list-cv-embauche.component';
 import { LoginComponent } from './login/login.component';
 import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
@@ -26,6 +26,7 @@ import { MergeComponent } from './components/merge/merge.component';
 import { ProductsComponent } from './components/products/products.component';
 import { UpdateCvComponent } from './cv/update-cv/update-cv.component';
 import { AddCvComponent } from './cv/add-cv/add-cv.component';
+import { tokenInjectionInterceptorProvider } from './token-injection.interceptor';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,7 @@ import { AddCvComponent } from './cv/add-cv/add-cv.component';
     HttpClientModule,
     ReactiveFormsModule,
   ],
-  providers: [LoginComponent],
-  bootstrap: [AppComponent]
+  providers: [LoginComponent, tokenInjectionInterceptorProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/token-injection.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInjectionInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const cloneRequest = request.clone({
+        params: request.params.set('access_token', token),
+      });
+      return next.handle(cloneRequest);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
+export const tokenInjectionInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: TokenInjectionInterceptor,
+  multi: true,
+};
